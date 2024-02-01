a78b0fe2f52b0823809375d8a63812e7bf3bd7be
Hank Caswell <david.h.caswell@proton.me>
2024-01-31 20:13:24 -0500

    solved the problems

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,36 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+ 
+ for(let i = 0; i<arr.length; i++){
+  if (arr[i] === searchTerm){
+    return i 
+  }
+ }
+return undefined
 }
 
+bannaList = "bananas".split("")
+console.log(linearSearch('q', bannaList))
+console.log(linearSearch('b', bannaList))
+
+
+
+
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let answer = []
+  for(let i = 0; i<arr.length; i++){
+    if (arr[i] === searchTerm){
+      answer.push(i)
+    }
+  }
+if(answer.length === 0){
+  return undefined
 }
+
+  return answer;
+}
+
+
+console.log(globalLinearSearch('a', bannaList))
+console.log(globalLinearSearch('q', bannaList))
 
 module.exports = { linearSearch, globalLinearSearch };
